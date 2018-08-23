a3c08cf7ac7ac47c5519689d774dcf3e969b1bc2
amanregmi11 <42652443+amanregmi11@users.noreply.github.com>
2018-08-23 12:43:02 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Chris Bourke3 * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+ printf("Aman Regmi Computer science!\n");
+
+ return 0;
+}
